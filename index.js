43b641a6d3710b7a890816701e4f90fffe1265dc
+ <+>
2023-03-23 08:51:20 +0300

    completed phase1 firstclass functions lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  console.log(returnFirstTwoDrivers(drivers));
+  
+ 
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  console.log(returnLastTwoDrivers(drivers));
+  
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  function selectDifferentDrivers(drivers, diff) {
+    return diff(drivers);
+  }
